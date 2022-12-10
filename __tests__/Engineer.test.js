4fa858170d9cdc799afb2a3ce7e4d556e30b636f
Mathew Chandler <Cascabelrock@gmail.com>
2022-12-10 14:34:46 -0800

    added video to README

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,8 +1,9 @@
+// state variable containing require function for Node
 const Engineer = require("../lib/Engineer");
-
+// test for eng
 test("set up", () => {
   const value = "githubUser";
-  const eng = new Engineer("name", 1, "Joe@joe.com");
+  const eng = new Engineer("Joe", "1", "Joe@joe.com");
 
   expect(eng.getGithub()).toBe(value);
 });
